79d71ff1d525ba1f6f6842b06137ef810eb35db1
zbl14 <ifthereisoneday@gmail.com>
2022-07-12 11:33:44 -0700

    add GetAllergens method, test of GetAllergens(1), test of GetAllergens(3) passed

---
+++ Allergies.Tests/ModelTests/AllergiesTests.cs
@@ -0,0 +1,22 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Allergies;
+
+namespace Allergies.Tests
+{
+ [TestClass]
+ public class AllergiesTests
+ {
+  [TestMethod]
+  public void AllergiesTests_GetAllergens_Eggs()
+  {
+    int score = 1;
+    Assert.AreEqual("eggs", Allergies.GetAllergens(score));
+  }
+  [TestMethod]
+  public void AllergiesTests_GetAllergens_EggsPeanuts()
+  {
+    int score = 3;
+    Assert.AreEqual("eggs, peanuts", Allergies.GetAllergens(score));
+  }
+ }
+}
---
+++ Allergies/Model/Allergies.cs
@@ -0,0 +1,51 @@
+using System;
+using System.Collections.Generic;
+
+namespace Allergies
+{
+  public class Allergies
+  {
+    public string Allergen { get; set; }
+    public int Score { get; set; }
+
+    public Allergies(string allergen, int score)
+    {
+      Allergen = allergen;
+      Score = score;
+    }
+
+    public static string GetAllergens(int score)
+    {
+      Allergies eggs = new Allergies("eggs", 1);
+      Allergies peanuts = new Allergies("peanuts", 2);
+      Allergies shellfish = new Allergies("shellfish", 4);
+      Allergies strawerries = new Allergies("strawerries", 8);
+      Allergies tomatoes = new Allergies("tomatoes", 16);
+      Allergies chocolate = new Allergies("chocolate", 32);
+      Allergies pollen = new Allergies("pollen", 64);
+      Allergies cats = new Allergies("cats", 128);
+
+      List<Allergies> ListOfAllergies = new List<Allergies>() { eggs, peanuts, shellfish, strawerries, tomatoes, chocolate, pollen, cats };
+      int indice = score;
+      string wantedallergen = "";  
+      List<string> listOfResult = new List<string>(0);
+      while(indice >= 1)
+      {
+        int min = 0;
+        int wanted = 0;
+        foreach(Allergies allergen in ListOfAllergies)
+        {
+          if(allergen.Score > min && allergen.Score <= indice)
+          {
+            wanted = allergen.Score;
+            min = allergen.Score;
+            wantedallergen = allergen.Allergen;
+          }
+        }
+        listOfResult.Insert(0, wantedallergen);
+        indice -= wanted;
+      }
+      return String.Join(", ", listOfResult);
+    }
+  }
+}
