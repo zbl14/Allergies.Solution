29c5d0a34a2336b7c2b378757a6d00fa1faf1a21
zbl14 <ifthereisoneday@gmail.com>
2022-07-12 12:03:04 -0700

    WIP: working on namespace

--- Allergies/Program.cs
+++ Allergies/Program.cs
@@ -1,23 +1,14 @@
-using System;
-using Allergies.Modles;
+// using System;
+// using Allergies.Modles;
 
-namespace AllergiesList
-{
-  public class Program
-  {
-   public static void Main() 
-   {
-    Allergies eggs = new Allergies ("eggs", 1);
-    Allergies peanuts = new Allergise ("peanuts", 2);
-    Allergies shellfish = new Allergise ("shellfish", 4);
-    Allergies strawerries = new Allergise ("strawerries", 8);
-    Allergies tomatoes = new Allergise ("tomatoes", 16);
-    Allergies chocolate = new Allergise ("chocolate", 32);
-    Allergies pollen = new Allergise ("pollen", 64);
-    Allergies cats = new Allergise ("cats", 128);
+// namespace AllergiesList
+// {
+//   public class Program
+//   {
+//    public static void Main() 
+//    {
 
-    List<Allergies> Allergies = new List<Allergies>() {eggs, peanuts, shellfish, strawerries, tomatoes, chocolate, pollen, cats}
-   }
-  }
-}
+//    }
+//   }
+// }
 
